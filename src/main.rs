48a41e1d79c8d3a315e7b673f77458da4a91f077
Dev Patel <hifromdev@gmail.com>
2023-11-06 12:54:54 -0500

    Base functionality for change_view(). Still unfunctional.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use rpassword;
 use json;
 
 
-// View stores the data related to different pages in the application.
+// 'View' stores the relationship between parent-child views.
+// Specifically, 'View' stores the contents of a page and its links to children pages.
 struct View {
     title: String,
     options: Vec<View>,
@@ -19,14 +20,29 @@ fn build_view(title: String, options: Vec<View>, has_field: bool) -> View {
     }
 }
 
-// Changes application text as the user navigates through the application.
+// This changes the contents of the application.
 fn change_view(current_view: &View, choice: &u8) -> View {
     // Add code to clear and print new pages based on the selected View.
+
+    let new_view: View = current_view.options[choice];
+
+    print!("\x1b[2J\x1b[1;1H"); // Clears the page and sets the cursor to one line below the top line. (Unable to place it at 0,0)
+    io::stdout().flush().unwrap();
+
+    // if not has_field... then do below. else has_field prompt. Add this code.
+    println!("{}", new_view.title);
+    println!("[0] Exit");
+    for n in 0..new_view.options.len()-2 {
+        println!("[{n+1}] {new_view[n].title}");
+    }
+    
+    return new_view;
+
 }
 
-// This function processes the user input on a given page,
-// checks if the input is valid, and opens a child loop 
-// when it navigates into another view.
+// This function processes the user input within a view,
+// checks if the input is valid, and, if required, opens 
+// a child loop for another view.
 fn process_input(current_view: &View, prompt: String) {
 
     loop {
@@ -47,11 +63,11 @@ fn process_input(current_view: &View, prompt: String) {
             break;
         }
 
-        process_input(&change_view(current_view, choice), prompt);
+        process_input(&change_view(current_view, &prompt[..1].parse().unwrap()), prompt);
 
         // Breaks all loops
         if prompt == "-1\n" { 
-            break 'app; 
+            break; 
         }
 
     }
@@ -67,14 +83,16 @@ fn main() {
     let key: String = rpassword::read_password().unwrap();
     if key != "asdf" { std::process::exit(1) } // This is temporary.
 
-    // let home: View = build_view("Home", []);
-    // let add_item: View = build_view("Add item", [])
+    let access_item: View = build_view("Access item".to_string(), vec![], false);
+    let add_item: View = build_view("Add item".to_string(), vec![], false);
+    let home: View = build_view("Home".to_string(), vec![add_item, access_item], false);
+    
 
     let mut prompt: String = String::new();
 
     
-
-    println!("\n[1] Add an item\n[2] Access an item\n[0] Exit\n");
+    // Redesign this section and make it tidier.
+    println!("\n[1] Add an item\n[2] Access an item\n[-1] Exit\n");
 
     loop {
 
@@ -85,7 +103,7 @@ fn main() {
             .read_line(&mut prompt)
             .expect("Failed to read line.");
 
-        process_input(home, prompt)
+        process_input(&home, prompt);
 
         if prompt == "-1\n" { 
             break; 
